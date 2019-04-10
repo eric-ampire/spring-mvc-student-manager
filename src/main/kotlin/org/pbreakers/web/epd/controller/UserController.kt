6fbb12c73929c0d103c8dabba652662c6baadce5
Eric Ampire <ericampire.top@gmail.com>
2019-04-10 10:02:17 +0200

    [ADD] Controller user

---
+++ src/main/kotlin/org/pbreakers/web/epd/controller/UserController.kt
@@ -0,0 +1,24 @@
+package org.pbreakers.web.epd.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class UserController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "page/home"
+    }
+
+    @GetMapping("/loginView")
+    fun login(): String {
+        return "page/login"
+    }
+
+    @GetMapping("/createAccountView")
+    fun createAccount(): String {
+        return "page/createAccount"
+    }
+}
