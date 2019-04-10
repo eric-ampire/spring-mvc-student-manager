5ccbbac23eb58c7b59a1d6f31e1b0a00098863c3
Eric Ampire <ericampire.top@gmail.com>
2019-04-10 19:19:52 +0200

    Init Project

--- src/main/kotlin/org/pbreakers/web/epd/database/UserRepository.kt
+++
@@ -1,6 +0,0 @@
-package org.pbreakers.web.epd.database
-
-import org.pbreakers.web.epd.entities.User
-import org.springframework.data.repository.CrudRepository
-
-abstract class UserRepository : CrudRepository<String, User>
