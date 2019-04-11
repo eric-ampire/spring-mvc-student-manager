5ff24f499d1bc87acd8f1d3dae7d6801c5d82c62
Eric Ampire <ericampire.top@gmail.com>
2019-04-11 20:33:26 +0200

    [UPDATE] Get studiant page by page

--- src/main/kotlin/org/pbreakers/web/epd/controller/web/EtudiantController.kt
+++ src/main/kotlin/org/pbreakers/web/epd/controller/web/EtudiantController.kt
@@ -2,9 +2,11 @@ package org.pbreakers.web.epd.controller.web
 
 import org.pbreakers.web.epd.dao.EtudiantRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/etudiants")
@@ -16,12 +18,14 @@ class EtudiantController {
 
     // Pour acceder a ce l'index il faut utiliser l'url "localhost:port/etudiants/index"
     @RequestMapping("/index")
-    fun index(model: Model): String {
-        val etudiants = etudiantRepository.findAll()
+    fun index(model: Model, @RequestParam(name = "page", defaultValue = "0") page: Int): String {
+
+        // Get page of student
+        val pageEtudiants = etudiantRepository.findAll(PageRequest(page, 5))
 
         // Avant de retourner la vue, on ajoute les informations dans le model
         // comme ca ces information serons disponible dans la vue
-        model.addAttribute("etudiants", etudiants)
+        model.addAttribute("etudiants", pageEtudiants)
         return "index"
     }
 }
