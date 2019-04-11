9e24baab1f8e98054847cc1063330df045e91891
Eric Ampire <ericampire.top@gmail.com>
2019-04-11 22:09:22 +0200

    [UPDATE] Adding row data in the database

--- src/main/kotlin/org/pbreakers/web/epd/SpringDemoApplication.kt
+++ src/main/kotlin/org/pbreakers/web/epd/SpringDemoApplication.kt
@@ -1,11 +1,22 @@
 package org.pbreakers.web.epd
 
+import org.pbreakers.web.epd.dao.EtudiantRepository
+import org.pbreakers.web.epd.entities.Etudiant
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.domain.PageRequest
+import java.text.SimpleDateFormat
+import java.util.*
 
 @SpringBootApplication
 class SpringDemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringDemoApplication>(*args)
+	val ctx = runApplication<SpringDemoApplication>(*args)
+	val etudiantRepository = ctx.getBean(EtudiantRepository::class.java)
+
+	for (a in 1..30) {
+		val a = Etudiant(a.toLong(), "Eric Ampire $a", Date(), "eric$a@gmail.com", "photo$a.jpg")
+		etudiantRepository.save(a)
+	}
 }
