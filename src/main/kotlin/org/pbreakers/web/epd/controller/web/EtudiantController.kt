a66383d2fe3e6780127e76fb047382da6a635fa3
Eric Ampire <ericampire.top@gmail.com>
2019-04-11 20:31:16 +0200

    [UPDATE] Index logic

--- src/main/kotlin/org/pbreakers/web/epd/controller/web/EtudiantController.kt
+++ src/main/kotlin/org/pbreakers/web/epd/controller/web/EtudiantController.kt
@@ -3,16 +3,25 @@ package org.pbreakers.web.epd.controller.web
 import org.pbreakers.web.epd.dao.EtudiantRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("/etudiants")
 class EtudiantController {
 
     @Autowired
     private lateinit var etudiantRepository: EtudiantRepository
 
-    @RequestMapping("/")
-    fun index(): String {
+
+    // Pour acceder a ce l'index il faut utiliser l'url "localhost:port/etudiants/index"
+    @RequestMapping("/index")
+    fun index(model: Model): String {
+        val etudiants = etudiantRepository.findAll()
+
+        // Avant de retourner la vue, on ajoute les informations dans le model
+        // comme ca ces information serons disponible dans la vue
+        model.addAttribute("etudiants", etudiants)
         return "index"
     }
 }
