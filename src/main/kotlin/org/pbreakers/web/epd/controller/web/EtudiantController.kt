196fe839422384b170415a65ea04ece960dcbd57
Eric Ampire <ericampire.top@gmail.com>
2020-03-05 12:24:47 +0200

    Removing deprecated element

--- src/main/kotlin/org/pbreakers/web/epd/controller/web/EtudiantController.kt
+++ src/main/kotlin/org/pbreakers/web/epd/controller/web/EtudiantController.kt
@@ -3,16 +3,17 @@ package org.pbreakers.web.epd.controller.web
 import org.pbreakers.web.epd.dao.EtudiantRepository
 import org.pbreakers.web.epd.entities.Etudiant
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-@RequestMapping("/etudiants")
 class EtudiantController {
 
     @Autowired
@@ -20,38 +21,34 @@ class EtudiantController {
 
 
     // Pour acceder a ce l'index il faut utiliser l'url "localhost:port/etudiants/index"
-    @RequestMapping("/index")
+    @RequestMapping("/")
     fun index(model: Model,
               @RequestParam(name = "page", defaultValue = "0") page: Int,
               @RequestParam(name = "motCle", defaultValue = "") motCles: String): String {
 
         // Get page of student
-        val pageEtudiants = etudiantRepository.chercherEtudiant("%$motCles%", PageRequest(page, 4))
+        val pageEtudiants: Page<Etudiant> = etudiantRepository.findAll(PageRequest.of(page, 4))
 
         // List content the number of each page
         val pagesNumber = (0 until pageEtudiants.totalPages).toList()
 
-        // Avant de retourner la vue, on ajoute les informations dans le model
-        // comme ca ces information serons disponible dans la vue
-        model.addAttribute("pageEtudiants", pageEtudiants)
-        model.addAttribute("pageNumber", pagesNumber)
-        model.addAttribute("currentPage", page)
-        model.addAttribute("mc", motCles)
+
+        model["pageEtudiants"] = pageEtudiants
+        model["pageNumber"] =  pagesNumber
+        model["currentPage"] = page
+        model["mc"] = motCles
         return "index"
     }
 
     @RequestMapping("/form")
     fun showForm(model: Model): String {
-        val etudiant = Etudiant()
-
-        model.addAttribute("etudiant", etudiant)
+        model["etudiant"] = Etudiant()
         return "form"
     }
 
     @PostMapping("/saveEtudiant")
     fun save(etudiant: Etudiant): String {
         etudiantRepository.save(etudiant)
-
-        return "redirect:index"
+        return "redirect:/"
     }
 }
--- src/main/kotlin/org/pbreakers/web/epd/dao/EtudiantRepository.kt
+++ src/main/kotlin/org/pbreakers/web/epd/dao/EtudiantRepository.kt
@@ -5,16 +5,11 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import java.util.*
 
-interface EtudiantRepository : JpaRepository<Etudiant, Long> {
-    fun findByNom(nom: String): List<Etudiant>
-    fun findByNom(nom: String, pageable: Pageable): Page<Etudiant>
+interface EtudiantRepository : PagingAndSortingRepository<Etudiant, Long> {
 
-    @Query("SELECT e from Etudiant e where e.nom like :x")
-    fun chercherEtudiant(@Param("x") mc: String, pageable: Pageable): Page<Etudiant>
-
-    @Query("SELECT e from Etudiant e where e.dateNaissance > :debut and e.dateNaissance < :fin")
-    fun chercherEtudiant(@Param("debut") dateDebut: Date, @Param("fin") dateFin: Date): List<Etudiant>
 }
